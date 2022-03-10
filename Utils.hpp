53b56368b0426469d46a66eedd7c0eb92794ff53
Bryan K <78167383+BryanProg@users.noreply.github.com>
2022-03-10 01:05:57 -0300

    Adding some settings

--- Utils.hpp
+++ Utils.hpp
@@ -15,5 +15,15 @@ namespace Pandora
                                std::is_same_v<std::decay_t<T>, uint16_t> ||
                                std::is_same_v<std::decay_t<T>, uint32_t> ||
                                std::is_same_v<std::decay_t<T>, uint64_t>;
+
     
+    template <typename iter>
+	using iter_category_t = typename std::iterator_traits<iter>::iterator_category;
+
+	template <typename iter, typename category = std::input_iterator_tag>
+	constexpr inline bool is_iterator_v = std::is_convertible_v<iter_category_t<std::decay_t<iter>>, category>;
+
+    template<typename T>
+    using Difference_iterator_t = typename std::iterator_traits<T>::difference_type;
+
 }
