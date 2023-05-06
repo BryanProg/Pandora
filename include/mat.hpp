cb7feb7ea0308e66d77faedda3a7a51da2cd0fb3
Bryan K <contato.bryankeven@gmail.com>
2023-05-06 15:32:10 -0300

    create module matrix

---
+++ include/mat.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+#include <utils.hpp>
+#include <cstdint>
+
+namespace Pandora::Mat
+{
+
+	//Mat<row, column, type>
+
+	template <uint8_t R, uint8_t C, typename T>
+	class mat
+	{
+
+	};
+}
